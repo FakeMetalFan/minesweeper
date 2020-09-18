f7b10bfb29577df67a96b075e0bbe28063a8a524
FakeMetalFan <fakemetalfan@gmail.com>
2020-09-18 16:29:58 +0300

    Updates

---
+++ precache-manifest.34d923d36712245390877c99f70a1c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0699afc0b4dcd4e15d1ca16825c1672",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "857c1cbcd24e5c189efa",
+    "url": "/minesweeper/static/css/main.b571733d.chunk.css"
+  },
+  {
+    "revision": "4011cc09292a8f5ac8ed",
+    "url": "/minesweeper/static/js/2.42627e50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.42627e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857c1cbcd24e5c189efa",
+    "url": "/minesweeper/static/js/main.4f1688c1.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
