3988ce45347e0e559ff36e5bd0d04a045a272560
FakeMetalFan <fakemetalfan@gmail.com>
2020-09-19 10:35:34 +0300

    Feature: Refactor: simplified imports;

--- src/view-models/field-producer.js
+++ src/view-models/field-producer.js
@@ -6,8 +6,7 @@ import difference from 'lodash/difference';
 
 import { cellValue, cellState } from 'const';
 
-import { Cell } from './internals/cell';
-import { CellNeighborsUtils } from './internals/cell-neighbors-utils';
+import { Cell, CellNeighborsUtils } from './internals';
 
 export class FieldProducer {
   constructor(
