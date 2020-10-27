bd3e1fac6803d32a9fbe960072aada8098086ef8
FakeMetalFan <fakemetalfan@gmail.com>
2020-10-27 22:42:05 +0200

    Updates

--- precache-manifest.111f338201c50c8220effb3b46078c24.js
+++ precache-manifest.111f338201c50c8220effb3b46078c24.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d451eea3a68cf239e03a37ed9649b37d",
+    "revision": "82b250c0f8d4f8df1b33e2848172a0b2",
     "url": "/minesweeper/index.html"
   },
   {
-    "revision": "b4f8c6fb9a7e92ac309f",
-    "url": "/minesweeper/static/css/main.41f225be.chunk.css"
+    "revision": "344a7392b767219fbbd0",
+    "url": "/minesweeper/static/css/main.e9aa710b.chunk.css"
   },
   {
     "revision": "440dc27c06c7abbd93fd",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/minesweeper/static/js/2.0abecde5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b4f8c6fb9a7e92ac309f",
+    "revision": "344a7392b767219fbbd0",
     "url": "/minesweeper/static/js/main.8fe00b37.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minesweeper/precache-manifest.8b403c83df45971ea48be9a6081ea486.js"
+  "/minesweeper/precache-manifest.111f338201c50c8220effb3b46078c24.js"
 );
 
 self.addEventListener('message', (event) => {
