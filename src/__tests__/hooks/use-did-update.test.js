da7e6e3df13d0585f8d8c11e3163c8f748487406
FakeMetalFan <fakemetalfan@gmail.com>
2020-10-31 09:51:37 +0200

    Feature: simplified useDidUpdate unit test;

--- src/__tests__/hooks/use-did-update.test.js
+++ src/__tests__/hooks/use-did-update.test.js
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { useDidUpdate } from 'hooks';
 
 describe('useDidUpdate', () => {
-  const MockComponent = ({ str }) => {
-    const [text, setText] = useState(str);
+  const fn = jest.fn();
 
-    useDidUpdate(() => {
-      setText(str);
-    }, str);
+  const MockComponent = ({ prop }) => {
+    useDidUpdate(fn, prop);
 
-    return text;
+    return null;
   };
 
-  const value = 'value';
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('should not fire when component mounts', () => {
-    render(<MockComponent str={value} />);
+    render(<MockComponent prop={1} />);
 
-    expect(screen.getByText(value)).toBeInTheDocument();
+    expect(fn).not.toHaveBeenCalled();
   });
 
   it('should fire when component updates', () => {
-    const { rerender } = render(<MockComponent str={value} />);
-    const newValue = 'new-value';
+    const { rerender } = render(<MockComponent prop={1} />);
 
-    rerender(<MockComponent str={newValue} />);
+    rerender(<MockComponent prop={2} />);
 
-    expect(screen.getByText(newValue)).toBeInTheDocument();
+    expect(fn).toHaveBeenCalled();
   });
 });
